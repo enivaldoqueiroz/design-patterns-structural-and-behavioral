796b2f3661183d757c54f815377b7111e89a281b
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2025-08-31 16:57:46 +0100

    Outros commitis

---
+++ Proxy2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Proxy2;
+
+Console.WriteLine(); 
+
+var flag = true;
+while (flag)
+{
+    Console.WriteLine("Informe a url do site: ");
+    var url = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(url))
+    {
+        try
+        {
+            var internet = new ProxyInternet();
+            internet.Conexao(url);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    else
+    {
+        flag = false;
+    }
+
+    Console.WriteLine("Final de processamente");
+    Console.ReadLine();
+}
