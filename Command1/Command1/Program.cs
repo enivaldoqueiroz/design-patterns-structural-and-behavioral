267a1915056e637f0cff2363fbd8a8aee6d10c04
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2025-09-03 20:01:49 +0100

    Implementação do Command

---
+++ Command1/Command1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Command1;
+
+Console.WriteLine("## Padrão Command ##");
+//Cria uma instancia de Chef para poder preparar o pedido
+//Receiver
+Chef chef = new Chef();
+
+//Prato
+Pedido pedido = new Pedido(chef: chef, acao: "Prato");
+Garcom garcom = new Garcom(pedido: pedido);
+garcom.Excutar();
+
+//Sobremesa
+pedido = new Pedido(chef: chef, acao: "Sobremesa");
+garcom = new Garcom(pedido: pedido);
+garcom.Excutar();
+
+Console.ReadKey();
