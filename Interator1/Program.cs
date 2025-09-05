3649a93696f319b816bf5cc14acf391b61ebb4bb
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2025-09-05 20:11:16 +0100

    Implementação do Padrão Interator1

---
+++ Interator1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Interator1;
+
+// Cria uma coleção
+ConcreteCollection colecao = new ConcreteCollection();
+
+colecao.AddCliente(new Cliente("Pedro", 10));
+colecao.AddCliente(new Cliente("Maria", 11));
+colecao.AddCliente(new Cliente("Manoel", 12));
+colecao.AddCliente(new Cliente("Miriam", 13));
+colecao.AddCliente(new Cliente("Jefferson", 14));
+
+// Cria o iterator que vai encapsular a iteração
+Iterator iterator = colecao.CreateIterator();
+
+Console.WriteLine("### Usando o padrão Iterador ###\n");
+Console.WriteLine("Pressione algo para iniciar");
+Console.ReadKey();
+
+//  iterator      
+Console.WriteLine("Iterando sobre a coleção de Clientes :\n ");
+
+for (Cliente cliente = iterator.First(); !iterator.IsDone; cliente = iterator.Next())
+{
+    Console.WriteLine($"ID : {cliente.Id} & Nome : {cliente.Nome}");
+}
+Console.Read();
